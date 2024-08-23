2a32807b7417a99e005fcb469c3c79a1febb58ed
Kleberson Gabriel <gabrielkleberson03@gmail.com>
2024-08-23 00:14:39 -0300

    jogo da cobrinha finalizado

--- script/script.js
+++ script/script.js
@@ -1,24 +1,35 @@
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
+let canvas = document.getElementById("snake"); 
+let context = canvas.getContext("2d");
 let box = 32;
-let snake = [];
-snake[0]= {
+
+let snake = []; 
+snake[0] = {
     x: 8 * box,
     y: 8 * box
-}
+};
 
 let direction = "right";
 
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-function cobrinha(){
-    for(i=0; i < snake.length; i++ ){
-        context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+function createBG() {
+    context.fillStyle = "lightgreen";
+    context.fillRect(0,0,16 * box, 16 * box);
+}
+
+function createSnake() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "black";
+        context.fillRect(snake[i].x, snake[i].y, box-2, box-2);
     }
+}
+
+function createFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -30,26 +41,44 @@ function update(event) {
     if (event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
-function iniciarJogo(){
-    criarBG();
-    cobrinha();
+function startGame() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game Over :(');
+        }
+    }
+
+    createBG();
+    createSnake();
+    createFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
-    
-    snake.pop();
+    if( direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
-        x: snakeX, 
-        y: snakeY 
-     
-    }
-    snake.unshift(newHead);
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(newHead); 
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let game = setInterval(startGame, 100); 
